0e8d34b322f312a6797604aff3e2a6f2c256f9a0
dat ngo <dat.ngophat@hdwebsoft.co>
2019-04-16 09:50:47 +0700

    Test codepush build

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        Text
       </View>
     );
   }
